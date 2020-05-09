a11ed6ddaa8210748ecf4791087c9e6354625986
jaydenkwright <jaydenkwright3@gmail.com>
2020-05-08 20:55:50 -0400

    User access token can be set into a httpOnly cookie sent from the server to the client

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
+
 router.get('/callback', async (req, res) => {
     const { code, state } = req.query
     try{
+        if (req.cookies.access_token) res.clearCookie('access_token')
         const response = await axios.post(
-          `https://www.figma.com/api/oauth/token?client_id=XHqtRRkNPxiFkkosx4ZcOY&client_secret=hAOb2JxuDOKvkRZNL7CWyKK8lXsjRn&redirect_uri=http://localhost:5000/auth/callback&code=${code}&grant_type=authorization_code`)
-          console.log(response.data)
-          res.json(response.data)
-      }catch(err){
+        `https://www.figma.com/api/oauth/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&redirect_uri=${process.env.CALLBACK}&code=${code}&grant_type=authorization_code`)
+        const { access_token, refresh_token, expires_in } = response.data
+        console.log(response.data)
+        res.cookie('access_token', access_token, {
+            httpOnly: true
+        })
+        console.log(response.data.access_token)
+        res.json(response.data.access_token)
+    }catch(err){
         console.log(err)
-      }
+    }
 })
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config()
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const auth = require('./routes/auth')
 const figma = require('./routes/figma')
 const app = express()
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:3000',
